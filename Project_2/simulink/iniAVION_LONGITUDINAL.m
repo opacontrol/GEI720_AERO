5e21b2bf3c906f0dd3085e0c34a69ce7d62b5864
Oussama <oussrock@gmail.com>
2016-11-03 13:12:33 -0400

    add project 2

---
+++ Project_2/simulink/iniAVION_LONGITUDINAL.m
@@ -0,0 +1,95 @@
+% Programme d'initialisation des parametres de la dynamique de l'avion
+%
+% JdeL 24 fevrier 2003
+% GEI-720
+
+% Constantes et conditions initiales 
+% ----------------------------------
+
+% Constantes
+
+  d2r_cst = pi/180.0;		    % degres en radians
+  r2d_cst = 1.0/d2r_cst;	    % radians en degres
+  r2d_1x3 = [1,1,1]*r2d_cst;	% matrice-rangee de 3 constantes
+  d2r_1x3 = [1,1,1]*d2r_cst;	% matrice-rangee de 3 constantes
+
+  g_cst = 9.78;			        % m/s^2
+
+% Altitude et position initiales. On choisit la position "nord" a zero
+% et l'altitude a 10 000 metres.
+% La matrice-rangee pos_ini apparait dans le bloc INTEG_POS.
+
+  h_ini   = 10000.0;	    % m
+  pN_ini  = 0.0;		    % m
+  pos_ini = [h_ini, pN_ini];
+
+% 		Sous-systeme 'DYN' 
+% 		------------------
+
+% Parametres d'inertie
+
+  ACmass       = 90000.0;	% kg
+  ACinertia_yy =  8000.0;	% kg m^2
+
+% 		Sous-systeme 'AERO' 
+% 		-------------------
+
+% Parametres aerodynamiques
+
+  cbar =   5.0;		% m
+  Sref = 200.0; 	% m^2
+  rho  = 1.225;		% kg/m^3 densite de l'air
+ 
+  CD0    = 0.018;
+  CD1    = 0.009;
+  CD2    = 0.040;
+
+  CL0    = 0.20;
+  CLalfa = 0.20;
+
+  CM0     =  0.050;
+  CMalfa  = -0.001;
+  CMdelta = -0.016;
+  CMq     = -0.300;
+
+% 		Sous-systeme 'PROPULSION' 
+% 		-------------------------
+
+% Parametres des moteurs
+
+  thrust_max = 108000.0;	% Newtons (pour le cas ou FT_V = 0)
+
+  FT80   = thrust_max ;
+  FT_V   = -200.0;
+
+% Offset entre ligne de poussee et centre de masse
+
+  xt = -0.3;			% m
+
+% ATTENTION: LES DONNEES CI-DESSOUS DOIVENTS ETRE CHANGEES POUR L'EQUILIBRE A 80 m/s
+% L'equilibre choisi en exemple ici est un vol horizontal a 70 m/s.
+% L'equilibre choisi pour le projet, et illustre dans le test de validation, est a 80 m/s.
+% Vous pouvez utiliser les resultats ci-dessous pour valider votre solution TRIM a 70 m/s.
+% Pour comparer avec le fichier de validation, ne pas oublier (evidemment) de changer 
+% les valeurs a l'equilibre ci-dessous pour celles à 80 m/s.
+% 
+  VTe   = 70.00000;		% vitesse à l'équilibre en m/s
+  alfae =  0.10941;		% angle d'attaque à l'équilibre en rad (6.26845 deg)
+  tetae =  0.10941;		% angle de tangage à l'équilibre en rad (6.26845 deg)
+                        % alfae = tetae donc vol horizontal (gamae = 0.0)
+  qe    =  0.00000;		% 'pitch rate' est nul à l'équilibre
+
+  deltae = 2.29707;     % gouvernail de profondeur à l'équilibre
+  ae     = 0.63467;     % propulsion à l'équilibre
+  
+%   alfae_deg =  6.26845;		% angle d'attaque en deg
+%   tetae_deg =  6.26845;		% angle de tangage en deg
+
+  etat_equil = [VTe, alfae, tetae, qe]; % conditions d'equilibre 
+
+% Conditions initiales:
+% --------------------
+% On choisit la dynamique initiale a l'equilibre (pas tjrs le cas). 
+% Voir le bloc INTEG_DYN du modele Simulink ou elles apparaissent.
+
+  etat_ini = etat_equil;
---
+++ Project_2/simulink/runAVION_LONGITUDINAL.m
@@ -0,0 +1,95 @@
+% Programme qui simule et donne les resultats (étudiants)
+
+% Fermeture des fenetres d'affichage
+  clear
+  close all
+  clc
+
+% SIMULATION
+% ----------
+
+% Initialisation du modèle SIMULINK de l'avion longitudinal
+  iniAVION_LONGITUDINAL
+
+% Definition de la matrice-colonne temps
+
+  t = [0.0:0.05:3000]';
+
+% Execution de la simulation avec integrateur par defaut et
+% enregistrement des resultats dans la matrice 'y'.
+% Il y a autant de colonnes dans y qu'il y a de sorties dans
+% le modele 'AVION_LONGITUDINAL'. Il y a une rangee pour chaque
+% valeur de temps dans 't'.
+
+  [tsim,x,y]         = sim('AVION_LONGITUDINAL',t);
+
+% GRAPHIQUES
+% ----------
+
+  figure(1)
+  subplot(2,1,1)
+  plot(t, [y(:,1)],'LineWidth',2), grid
+%   hold on
+%   plot(t, [y(:,17)],'c','LineWidth',2)
+  xlabel('Temps (s)', 'FontWeight', 'bold')
+  ylabel('Vitesse (m/s)', 'FontWeight', 'bold')
+  title('Vitesse et angle d''attaque en fonction du temps', 'FontWeight', 'bold')
+  
+  subplot(2,1,2)
+  plot(t, [y(:,2)],'LineWidth',2), grid
+%   hold on
+%   plot(t, [y(:,18)],'c','LineWidth',2);
+  xlabel('Temps (s)', 'FontWeight', 'bold')
+  ylabel('Angle d''attaque (deg)', 'FontWeight', 'bold')
+  
+  figure(2)
+  subplot(2,1,1)
+  plot(t, [y(:,3)],'LineWidth',2), grid
+%   hold on
+%   plot(t, [y(:,19)],'c','LineWidth',2)
+  xlabel('Temps (s)', 'FontWeight', 'bold')
+  ylabel('Angle de tangage (deg)', 'FontWeight', 'bold')
+  title('Angle et vitesse angulaire en tangage en fonction du temps', 'FontWeight', 'bold')
+
+  subplot(2,1,2)
+  plot(t, [y(:,4)],'LineWidth',2), grid
+%   hold on
+%   plot(t, [y(:,20)],'c','LineWidth',2)
+  xlabel('Temps (s)', 'FontWeight', 'bold')
+  ylabel('Vitesse angulaire en tangage (deg/s)', 'FontWeight', 'bold')
+
+  figure(3)
+  plot(t, [y(:,5)],'LineWidth',2), grid
+%   hold on
+%   plot(t, [y(:,21)],'c','LineWidth',2)
+  xlabel('Temps (s)', 'FontWeight', 'bold')
+  ylabel('Angle de vol (deg)', 'FontWeight', 'bold')
+  title('Angle de vol (''flight path angle'') en fonction du temps', 'FontWeight', 'bold')
+  text(1100, 2, 'L''avion descend', 'FontWeight', 'bold')
+  text( 100, 2, 'Vol horizontal', 'FontWeight', 'bold')
+
+  figure(4)
+  plot(y(:,16)/1000, y(:,15)/1000,'LineWidth',2), grid
+%   hold on
+%   plot(y(:,22)/1000, y(:,23)/1000,'c','LineWidth',2)
+  xlabel('Position horizontale (km)', 'FontWeight', 'bold')
+  ylabel('Altitude (km)', 'FontWeight', 'bold')
+  title('Altitude en fonction de la position horizontale', 'FontWeight', 'bold')
+   
+  figure(5)
+  plot(t, [y(:,4)],'-','LineWidth',2), grid
+%   hold on
+%   plot(t, [y(:,20)],'--','LineWidth',2)
+  V = axis;
+  axis([495, 540, V(3), V(4)])
+  xlabel('Temps (s)', 'FontWeight', 'bold')
+  ylabel('Vitesse angulaire en tangage (deg/s)', 'FontWeight', 'bold')
+  title('Zoom pour illustrer le mode ''short period'' (2s) et le mode phugoid (22s)', 'FontWeight', 'bold')
+  text(502, 18, 'periode = 1.3 s', 'FontWeight', 'bold')
+  text(516,  8, 'periode = 20 s', 'FontWeight', 'bold')
+  legend('Modèle non linéaire', 'Modèle linéarisé');
+  
+  
+  
+  
+  
