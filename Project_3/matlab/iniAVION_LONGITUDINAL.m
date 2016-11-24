8d5e48076f04c158cba8c3f002f3b44f0fb64a54
Sebastien Demers <sebastien.demers91@gmail.com>
2016-11-24 08:46:13 -0500

    format iniAVION_LONGITUDINAL.m

--- Project_3/matlab/iniAVION_LONGITUDINAL.m
+++ Project_3/matlab/iniAVION_LONGITUDINAL.m
@@ -3,7 +3,7 @@
 % JdeL 24 fevrier 2003
 % GEI-720
 
-% Constantes et conditions initiales 
+% Constantes et conditions initiales
 % ----------------------------------
 
 % Constantes
@@ -23,7 +23,7 @@
   pN_ini  = 0.0;		    % m
   pos_ini = [h_ini, pN_ini];
 
-% 		Sous-systeme 'DYN' 
+% 		Sous-systeme 'DYN'
 % 		------------------
 
 % Parametres d'inertie
@@ -31,7 +31,7 @@
   ACmass       = 90000.0;	% kg
   ACinertia_yy =  8000.0;	% kg m^2
 
-% 		Sous-systeme 'AERO' 
+% 		Sous-systeme 'AERO'
 % 		-------------------
 
 % Parametres aerodynamiques
@@ -39,7 +39,7 @@
   cbar =   5.0;		% m
   Sref = 200.0; 	% m^2
   rho  = 1.225;		% kg/m^3 densite de l'air
- 
+
   CD0    = 0.018;
   CD1    = 0.009;
   CD2    = 0.040;
@@ -52,7 +52,7 @@
   CMdelta = -0.016;
   CMq     = -0.300;
 
-% 		Sous-systeme 'PROPULSION' 
+% 		Sous-systeme 'PROPULSION'
 % 		-------------------------
 
 % Parametres des moteurs
@@ -70,16 +70,16 @@
 % L'equilibre choisi en exemple ici est un vol horizontal a 70 m/s.
 % L'equilibre choisi pour le projet, et illustre dans le test de validation, est a 80 m/s.
 % Vous pouvez utiliser les resultats ci-dessous pour valider votre solution TRIM a 70 m/s.
-% Pour comparer avec le fichier de validation, ne pas oublier (evidemment) de changer 
+% Pour comparer avec le fichier de validation, ne pas oublier (evidemment) de changer
 % les valeurs a l'equilibre ci-dessous pour celles à 80 m/s.
-% 
+%
 
 %   VTe   = 80.00000;		% vitesse à l'équilibre en m/s.
 %   alfae =  0.10941;		% angle d'attaque à l'équilibre en rad (6.26845 deg)
 %   tetae =  0.10941;		% angle de tangage à l'équilibre en rad (6.26845 deg)
 %                         % alfae = tetae donc vol horizontal (gamae = 0.0)
 %   qe    =  0.00000;		% 'pitch rate' est nul à l'équilibre
-% 
+%
 %   deltae = 2.29707;     % gouvernail de profondeur à l'équilibre
 %   ae     = 0.63467;     % propulsion à l'équilibre
 X0  = [80, 0, 0, 0]'; IX  = [1];
@@ -103,11 +103,11 @@ qe = Xe(4);
 deltae = Ue(1);
 ae = Ue(2);
 
-etat_equil = [VTe, alfae, tetae, qe]; % conditions d'equilibre 
+etat_equil = [VTe, alfae, tetae, qe]; % conditions d'equilibre
 
 % Conditions initiales:
 % --------------------
-% On choisit la dynamique initiale a l'equilibre (pas tjrs le cas). 
+% On choisit la dynamique initiale a l'equilibre (pas tjrs le cas).
 % Voir le bloc INTEG_DYN du modele Simulink ou elles apparaissent.
 
 etat_ini = [85, alfae, tetae, qe];
@@ -118,9 +118,9 @@ vp = eig(A)
 Wa = imag(vp)
 
 
-%% Projet 3 : 
+%% Projet 3 :
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Partie A du projet: 
+% Partie A du projet:
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Question P3-1
 % calcul des poles desiree selon
@@ -136,23 +136,26 @@ Poled_4 = -Zeta*Wn2 - Wn2*sqrt(1-(Zeta^2))*i;
 Poled = [Poled_1, Poled_2, Poled_3,Poled_4];
 
 % Question P3-2
-% COMMANDABILITÉ PAR RETOUR D’ÉTAT 
+% COMMANDABILITÉ PAR RETOUR D’ÉTAT
+%
 % Methode 1 :
 M = ctrb(A,B);
 M = rank (M);
-disp(['Le rang de la matrice M = ' num2str(M)]);
-disp(['La dimention de la matrice A = ' num2str(length(A))]);
+fprintf('\nLe rang de la matrice M = %d\n', M);
+fprintf('La dimention de la matrice A = %d\n', length(A));
+
 %Methode 2 :
 [P,V]=eig(A);
 M2 = inv(P)*B;
 disp('Aucune range de inv(P) * B est nulle ');
-disp(M2)
+disp(M2);
+
 %Methode 3 : Vérification s'il y a annulation pôle-zéro
-Bdelta = B(:,1); 
+Bdelta = B(:,1);
 Ddelta = D(:,1)
 [num,den] = ss2tf(A, Bdelta, C, Ddelta);
 
-[r c] = size(num);
+[r, c] = size(num);
   for i = 1:r
     Zero = roots(num(i,:));
   end
@@ -160,13 +163,13 @@ Ddelta = D(:,1)
  Pole = roots(den)
  disp('Tous les pôles sont différents des zéros:')
  disp('Resultat : complètement commandable')
-  
-% Question P3-4 : 
+
+% Question P3-4 :
 % Calcul de la matrice de gain K
 K = place(A,Bdelta,Poled);
 disp('Matrice K pour les poles desirees choisis ')
 disp(K)
-% dans mon cas a moi ca marche pas 
+% dans mon cas a moi ca marche pas
 disp('Verification que mon calcul marche eig(A-B*K)= P')
 dum1=eig(A-Bdelta*K)
 disp(P);
