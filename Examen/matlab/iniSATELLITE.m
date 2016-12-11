5229c13d0f0d6ef140bb4c98f13f04aac2272815
Sebastien Demers <sebastien.demers91@gmail.com>
2016-12-11 13:38:16 -0500

    P4-1 et P4-2

--- Examen/matlab/iniSATELLITE.m
+++ Examen/matlab/iniSATELLITE.m
@@ -1,6 +1,6 @@
 % Fichier d'initialisation de la dynamique en rotation du satellite
 
-% JdeL 
+% JdeL
 % Décembre 2011
 % Modified by:
 %   - bouo3102
@@ -15,14 +15,14 @@ RPM2RAD = 2*pi/60;
 % ------
 
 % Inertie et conditions initiales de la roue 1
- 
+
  RW1inertie     = 4.10E-04;
  RW1inertie_inv = 1/RW1inertie;
  wrw1_ini       = 20 * RPM2RAD;
  hrw1_ini       = RW1inertie*wrw1_ini;
  RW1axe_B       = [1 0 0]';
  hrw1_B_ini      = hrw1_ini*RW1axe_B;
- 
+
 
 % ROUE 2
 % ------
@@ -47,7 +47,7 @@ RPM2RAD = 2*pi/60;
  hrw3_ini       = RW3inertie*wrw3_ini;
  RW3axe_B       = [0 0 1]';
  hrw3_B_ini     = hrw3_ini*RW3axe_B;
- 
+
 % Paramètre des 3 roues ensembles
 % ------
 hrw_B_ini   = hrw1_B_ini + hrw2_B_ini + hrw3_B_ini;
@@ -65,3 +65,80 @@ hsc_B_ini       = SCinertie * wsc_B_ini;
 htot_B_ini      = SCinertie * wsc_B_ini + hrw_B_ini;
 qsc_I_ini       = [0 0 0 1]';
 
+%%%
+%%%  Serie P
+%%%
+
+%%% P4-1
+htot_B_e = [0, 0, 0]';
+hrw1_e = 0;
+hrw2_e = 0;
+hrw3_e = 0;
+epsilon_e = [0, 0, 0]';
+eta_e = 1;
+q_e = [epsilon_e; eta_e];
+
+hrw_B_e = hrw1_e*RW1axe_B +  hrw2_e*RW2axe_B + hrw3_e*RW3axe_B;
+wb = SCinertie_inv * (htot_B_e - hrw_B_e);
+
+htot_B_cross = [ 0          , -htot_B_e(3),  htot_B_e(2);
+                 htot_B_e(3), 0           , -htot_B_e(1);
+                -htot_B_e(2), htot_B_e(1) ,  0];
+
+wb_cross = [ 0    , -wb(3),  wb(2);
+             wb(3), 0     , -wb(1);
+            -wb(2), wb(1) ,  0];
+
+syms delta_htot_B ...
+     delta_Tex_B;
+delta_htot_B_dot = (htot_B_cross * SCinertie_inv - wb_cross) * ...
+                   delta_htot_B + eye(3) * delta_Tex_B;
+
+syms delta_Tc1 ...
+     delta_Tc2 ...
+     delta_Tc3;
+
+delta_hrw1_dot = delta_Tc1;
+delta_hrw2_dot = delta_Tc2;
+delta_hrw3_dot = delta_Tc3;
+
+syms wb1 wb2 wb3;
+w_b = [wb1; wb2; wb3];
+delta_epsilon_dot = (1/2) * w_b;
+
+
+%%% P4-2
+A = zeros(9,9);
+B = zeros(9,6);
+E = eye(3);
+
+A_htot    = htot_B_cross * SCinertie_inv - wb_cross;
+
+E(1:4:end)= hrw_B_e;
+A_hrw     = E;
+
+E(1:4:end) = wb;
+A_epsilon = E;
+
+A(1:3,1:3) = A_htot;
+A(4:6,4:6) = A_hrw;
+A(7:9,7:9) = A_epsilon;
+
+E = eye(3);
+B(1:3,1:3) = E;
+B(4:6,4:6) = E;
+
+%%% Show results
+SHOW_RESULTS = 1;
+if SHOW_RESULTS
+    delta_htot_B_dot
+    delta_hrw1_dot
+    delta_hrw2_dot
+    delta_hrw3_dot
+    delta_epsilon_dot
+
+    A
+    B
+end
+
+%%% P4-3
